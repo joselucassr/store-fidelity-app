0490e9da56ea329e4415b839542d35fa7bfaa6c2
Lucas <joselucasrs@gmail.com>
2022-01-13 06:14:49 -0300

    First page layout done

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,141 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Image from 'next/image';
+import { useEffect, useRef, useState } from 'react';
+
+import _devQRCode from '../devAssets/gradientQR.png';
+import AwardImage from '../images/award-solid.png';
+import WhatsappImage from '../images/whatsapp-brands.png';
+
+// const qrCode = new QRCodeStyling({
+//   width: 300,
+//   height: 300,
+//   image: '',
+//   dotsOptions: {
+//     gradient: {
+//       type: 'linear',
+//       rotation: 45,
+//       colorStops: [
+//         { offset: 0, color: '#ef709b' },
+//         { offset: 1, color: '#fa9372' },
+//       ],
+//     },
+//     type: 'classy',
+//   },
+//   imageOptions: {
+//     crossOrigin: 'anonymous',
+//     margin: 20,
+//   },
+//   cornersSquareOptions: {
+//     type: 'square',
+//   },
+//   cornersDotOptions: {
+//     type: 'square',
+//   },
+// });
 
 export default function Home() {
+  const [windowHeight, setWindowHeight] = useState('100vh');
+  const [phoneNumber, setPhoneNumber] = useState('(61) 91234-5678');
+  const ref = useRef(null);
+  const qrCodeRef = useRef(null);
+
+  useEffect(() => {
+    const innerHeight = window.innerHeight;
+    const innerWidth = window.innerWidth;
+    const sizeRatio = 0.66;
+    const backupRatio = 0.8;
+
+    let canvasSize = innerHeight * sizeRatio;
+
+    if (canvasSize > innerWidth) canvasSize = innerWidth * backupRatio;
+
+    // Dynamically import qr-code-styling only client-side
+    import('qr-code-styling').then(({ default: QRCodeStyling }) => {
+      qrCodeRef.current = new QRCodeStyling({
+        width: canvasSize,
+        height: canvasSize,
+        image: '',
+        dotsOptions: {
+          gradient: {
+            type: 'linear',
+            rotation: 45,
+            colorStops: [
+              { offset: 0, color: '#ef709b' },
+              { offset: 1, color: '#fa9372' },
+            ],
+          },
+          type: 'classy',
+        },
+        imageOptions: {
+          crossOrigin: 'anonymous',
+          margin: 20,
+        },
+        cornersSquareOptions: {
+          type: 'square',
+        },
+        cornersDotOptions: {
+          type: 'square',
+        },
+        backgroundOptions: {
+          color: 'none',
+        },
+        data: phoneNumber,
+      });
+
+      qrCodeRef.current.append(ref.current);
+      ref.current = null;
+
+      // qrCodeRef.current = qrCode;
+    });
+  }, []);
+
+  useEffect(() => {
+    qrCodeRef.current &&
+      qrCodeRef.current.update({
+        data: phoneNumber,
+      });
+  }, [phoneNumber]);
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <div
+      // style={{ minHeight: windowHeight }}
+      className='flex flex-col mx-auto w-screen max-w-sm'
+    >
+      <div
+        onClick={() => setPhoneNumber('(61) 91234-1234')}
+        className='mx-auto px-4 my-4'
+      >
+        <div className='backdrop-blur-sm px-4 py-4 bg-sky-200/25 rounded-xl'>
+          {/* <Image src={_devQRCode}></Image> */}
+          <div ref={ref} />
+          <p className='text-center font-mont font-bold text-nicePink'>
+            Toque para trocar de número
+          </p>
         </div>
-      </main>
+      </div>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <div className='flex flex-row mx-4 gap-x-4'>
+        <div className='basis-3/4 backdrop-blur-sm px-4 py-2 rounded-xl btn-gradient-2 flex flex-col '>
+          <span className='text-xl text-gradient font-mont'>Pontos:</span>
+          <p className='text-9xl text-center font-mont font-bold text-gradient my-auto'>
+            20
+          </p>
+        </div>
+
+        <div className='basis-1/4 backdrop-blur-sm px-4 py-4 bg-sky-200/25 rounded-xl text-sm text-center font-medium flex flex-col gap-y-4'>
+          <div className=''>
+            <div className='w-10 mx-auto'>
+              <Image src={WhatsappImage} />
+            </div>
+            <p className='text-mont text-gradient'>Faça seu pedido</p>
+          </div>
+          <div className=''>
+            <div className='w-10 mx-auto'>
+              <Image src={AwardImage} />
+            </div>
+            <p className='text-mont text-gradient'>Recompensas</p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        mont: 'Montserrat',
+      },
+      colors: {
+        nicePink: '#F29EA5',
+      },
+    },
+  },
+  plugins: [],
+};
