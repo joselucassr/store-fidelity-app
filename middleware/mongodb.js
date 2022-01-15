02c1108bad77e53a60be4126c7b9be8831721299
Lucas <joselucasrs@gmail.com>
2022-01-15 03:18:33 -0300

    Still searching for the error

--- middleware/mongodb.js
+++ middleware/mongodb.js
@@ -1,16 +1,21 @@
 import mongoose from 'mongoose';
 
 const connectDB = (handler) => async (req, res) => {
+  console.log('Check for entry on DB');
   if (mongoose.connections[0].readyState) {
     // Use current db connection
     return handler(req, res);
   }
-  // Use new db connection
-  await mongoose.connect(process.env.mongodburl, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-  });
-  return handler(req, res);
+  try {
+    // Use new db connection
+    await mongoose.connect(process.env.mongodburl, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    return handler(req, res);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export default connectDB;
