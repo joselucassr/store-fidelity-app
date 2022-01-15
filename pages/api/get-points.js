9dbd13a26f4cb4c42a2ee168067bd171b3db1fd0
Lucas <joselucasrs@gmail.com>
2022-01-15 02:58:13 -0300

    Logging to try and solve the API errors

--- pages/api/get-points.js
+++ pages/api/get-points.js
@@ -2,18 +2,32 @@ import connectDB from '../../middleware/mongodb';
 import Customer from '../../Models/Customer';
 
 const handler = async (req, res) => {
+  console.log('START');
   if (!req.method === 'POST')
     return res.status(422).send('req_method_not_supported');
+  console.log(
+    'if (!req.method === "POST") return res.status(422).send("req_method_not_supported");',
+  );
 
   const { phoneNumber } = req.body;
+  console.log('const { phoneNumber } = req.body;');
 
   if (!phoneNumber) return res.status(422).send('data_incomplete');
+  console.log(
+    'if (!phoneNumber) return res.status(422).send("data_incomplete");',
+  );
 
   try {
+    console.log('try {');
     let customer = await Customer.findOne({ phoneNumber });
+    console.log('let customer = await Customer.findOne({ phoneNumber });');
 
+    console.log(
+      'return res.status(200).send({ points: customer.pointsTotal });',
+    );
     return res.status(200).send({ points: customer.pointsTotal });
   } catch (error) {
+    console.log('return res.status(500).send(error.message)');
     return res.status(500).send(error.message);
   }
 };
