152384cab27484559ea5255cb6c48d0b8a2af17f
Lucas <joselucasrs@gmail.com>
2022-01-15 04:10:13 -0300

    Removing logs

--- middleware/mongodb.js
+++ middleware/mongodb.js
@@ -1,29 +1,17 @@
 import mongoose from 'mongoose';
 
 const connectDB = (handler) => async (req, res) => {
-  console.time('dbConnect');
-  console.log('Check for entry on DB');
   if (mongoose.connections[0].readyState) {
-    console.log('if (mongoose.connections[0].readyState) {');
     // Use current db connection
-    console.log('return handler(req, res);');
     return handler(req, res);
   }
   try {
-    console.log('try {');
-    console.timeLog('dbConnect');
     // Use new db connection
     mongoose.connect(process.env.mongodburl, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
-    console.timeLog('dbConnect');
 
-    console.log(`await mongoose.connect(process.env.mongodburl, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    });`);
-    console.log(`return handler(req, res);`);
     return handler(req, res);
   } catch (error) {
     console.log(error);
--- pages/api/get-points.js
+++ pages/api/get-points.js
@@ -2,35 +2,18 @@ import connectDB from '../../middleware/mongodb';
 import Customer from '../../Models/Customer';
 
 const handler = async (req, res) => {
-  console.time('handler');
-  console.log('START');
   if (!req.method === 'POST')
     return res.status(422).send('req_method_not_supported');
-  console.log(
-    'if (!req.method === "POST") return res.status(422).send("req_method_not_supported");',
-  );
 
   const { phoneNumber } = req.body;
-  console.log('const { phoneNumber } = req.body;');
 
   if (!phoneNumber) return res.status(422).send('data_incomplete');
-  console.log(
-    'if (!phoneNumber) return res.status(422).send("data_incomplete");',
-  );
 
   try {
-    console.timeLog('handler');
-    console.log('try {');
     let customer = await Customer.findOne({ phoneNumber });
-    console.timeLog('handler');
-    console.log('let customer = await Customer.findOne({ phoneNumber });');
 
-    console.log(
-      'return res.status(200).send({ points: customer.pointsTotal });',
-    );
     return res.status(200).send({ points: customer.pointsTotal });
   } catch (error) {
-    console.log('return res.status(500).send(error.message)');
     return res.status(500).send(error.message);
   }
 };
