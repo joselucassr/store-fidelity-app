de7cfd883652ed55cb14df491e6b87ea80353b39
Lucas <joselucasrs@gmail.com>
2022-01-15 03:35:36 -0300

    More Logs :c

--- middleware/mongodb.js
+++ middleware/mongodb.js
@@ -3,15 +3,24 @@ import mongoose from 'mongoose';
 const connectDB = (handler) => async (req, res) => {
   console.log('Check for entry on DB');
   if (mongoose.connections[0].readyState) {
+    console.log('if (mongoose.connections[0].readyState) {');
     // Use current db connection
+    console.log('return handler(req, res);');
     return handler(req, res);
   }
   try {
+    console.log('try {');
     // Use new db connection
     await mongoose.connect(process.env.mongodburl, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
+
+    console.log(`await mongoose.connect(process.env.mongodburl, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });`);
+    console.log(`return handler(req, res);`);
     return handler(req, res);
   } catch (error) {
     console.log(error);
